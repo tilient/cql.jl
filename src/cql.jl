ce59988ae22ab00858c320d9d2d81ab3c0570424
wiffel <wiffel@tilient.net>
2014-08-01 22:52:22 +0200

    renames

--- src/cql.jl
+++ src/cql.jl
@@ -19,7 +19,7 @@ end
 
 ##################################################################
 
-function cqlConnect(srv::String = "localhost", prt::Int = 9042)
+function connect(srv::String = "localhost", prt::Int = 9042)
   con = CQLConnection(srv, prt);
   con.socket = Base.connect(con.server, con.port);
   sendMessage(con, 0x01, {"CQL_VERSION" => "3.0.0"});
@@ -31,7 +31,7 @@ function cqlConnect(srv::String = "localhost", prt::Int = 9042)
   con 
 end
 
-function cqlDisconnect(con::CQLConnection)
+function disconnect(con::CQLConnection)
   while 0 < con.pending
     yield();
   end
@@ -304,42 +304,42 @@ end
 ### CQL Query
 ##################################################################
 
-function cqlQuery(con::CQLConnection, msg::String)
+function query(con::CQLConnection, msg::String)
   ## 'Normal' Synchronous Query
   ## Will wait for until all scheduled commands have been executed
   ## Will then send the query and wait for the result.
   ## The processed result is returned as an array or
   ## rows, which are themselves array with the values
   ## for the requested columns.
-  cqlSync(con);
-  cqlAsyncGetResult(cqlAsyncQuery(con, msg))
+  sync(con);
+  getResult(asyncQuery(con, msg))
 end
 
-function cqlCommand(con::CQLConnection, msg::String)
-  ## The same as 'cqlQuery', but we don't get the result back.
+function command(con::CQLConnection, msg::String)
+  ## The same as 'query', but we don't get the result back.
   ## Is a bit faster and uses less memory, because the
   ## reply from the server is neglected.
-  cqlSync(con);
-  cqlAsyncCommand(con, msg)
-  cqlSync(con);
+  sync(con);
+  asyncCommand(con, msg)
+  sync(con);
   nothing
 end
 
-function cqlAsyncQuery(con::CQLConnection, msg::String)
+function asyncQuery(con::CQLConnection, msg::String)
   ## An Asynchronous Query
   ## Will send the query to the server and returns 
   ## with a 'future'. After the server has processed the
   ## query and did send back the result, the 'future' will
   ## contain the result. This result can be fetched with
-  ## 'cqlAsyncGetResult', which gives back the result in 
-  ## the same format as 'cqlQuery'.
+  ## 'getResult', which gives back the result in 
+  ## the same format as 'query'.
   id, reply = nextReplyID(con);
   sendMessage(con, 0x07, msg, id);
   reply
 end
 
-function cqlAsyncCommand(con::CQLConnection, msg::String)
-  ## The same as 'cqlCommand', but asynchronous.
+function asyncCommand(con::CQLConnection, msg::String)
+  ## The same as 'command', but asynchronous.
   ## It sends of the command to the server instantly and
   ## neglects the response.
   ## This is the fastest way to execute cql commands, but
@@ -349,13 +349,13 @@ function cqlAsyncCommand(con::CQLConnection, msg::String)
   nothing
 end
 
-function cqlAsyncGetResult(reply)
-  ## To fetch the result from a call by cqlAsyncQuery.
+function getResult(reply)
+  ## To fetch the result from a call by asyncQuery.
   ## Will block if the result is not there yet.
   decodeMessage(take!(reply) ...)
 end
 
-function cqlSync(con)
+function sync(con)
   ## Not very usefull, but waits until all messages
   ## that were send to the server where processed and
   ## communicated back.
@@ -373,21 +373,21 @@ function test01()
   ## 'Example of a music service' from 
   ## the manual 'About CQL for Cassandra 2.0'
 
-  c = cqlConnect("tilient.net");
+  c = connect("tilient.net");
 
-  cqlCommand(c, "CREATE KEYSPACE demo 
+  command(c, "CREATE KEYSPACE demo 
                  WITH REPLICATION = {'class' : 'SimpleStrategy',
                                      'replication_factor' : 1};");
-  cqlCommand(c, "USE DEMO;");
+  command(c, "USE DEMO;");
 
-  cqlCommand(c, """
+  command(c, """
     CREATE TABLE songs ( id uuid PRIMARY KEY,
                 title text,
                 album text,
                 artist text,
                 data blob ); """);
 
-  cqlCommand(c, """
+  command(c, """
     CREATE TABLE playlists ( id uuid,
                              song_order int,
                              song_id uuid,
@@ -396,27 +396,27 @@ function test01()
                              artist text,
                              PRIMARY KEY (id, song_order)); """);
 
-  cqlCommand(c, """
+  command(c, """
     INSERT INTO playlists (id, song_order, song_id, 
                            title, artist, album)
            VALUES (62c36092-82a1-3a00-93d1-46196ee77204, 1,
                    a3e64f8f-bd44-4f28-b8d9-6938726e34d4, 
                    'La Grange', 'ZZ Top', 'Tres Hombres'); """);
-  cqlCommand(c, """
+  command(c, """
     INSERT INTO playlists (id, song_order, song_id, 
                            title, artist, album)
            VALUES (62c36092-82a1-3a00-93d1-46196ee77204, 2,
                    8a172618-b121-4136-bb10-f665cfc469eb, 
                    'Moving in Stereo', 'Fu Manchu', 
                    'We Must Obey'); """);
-  cqlCommand(c, """
+  command(c, """
     INSERT INTO playlists (id, song_order, song_id, 
                            title, artist, album)
            VALUES (62c36092-82a1-3a00-93d1-46196ee77204, 3,
                    2b09185b-fb5a-4734-9b56-49077de9edbf, 
                    'Outside Woman Blues', 'Back Door Slam', 
                    'Roll Away'); """);
-  cqlCommand(c, """
+  command(c, """
     INSERT INTO playlists (id, song_order, song_id, 
                             title, artist, album)
            VALUES (62c36092-82a1-3a00-93d1-46196ee77204, 4,
@@ -424,90 +424,90 @@ function test01()
                    'Ojo Rojo', 'Fu Manchu', 
                    'No One Rides for Free'); """);
 
-  println(cqlQuery(c, 
+  println(query(c, 
     "SELECT * FROM playlists;"));
  
-  cqlCommand(c, 
+  command(c, 
     "CREATE INDEX ON playlists(artist);");
                 
-  println(cqlQuery(c, 
+  println(query(c, 
     "SELECT * FROM playlists WHERE artist = 'Fu Manchu';"));
                 
-  println(cqlQuery(c, """
+  println(query(c, """
     SELECT * FROM playlists 
     WHERE id = 62c36092-82a1-3a00-93d1-46196ee77204 
     ORDER BY song_order DESC 
     LIMIT 50; """));
                     
-  cqlCommand(c, 
+  command(c, 
     "ALTER TABLE songs ADD tags set<text>;");
                     
-  cqlCommand(c, """
+  command(c, """
      UPDATE songs  SET tags = tags + {'2007'}
        WHERE id = 8a172618-b121-4136-bb10-f665cfc469eb; """); 
      
-  cqlCommand(c, """
+  command(c, """
      UPDATE songs  SET tags = tags + {'covers'}
        WHERE id = 8a172618-b121-4136-bb10-f665cfc469eb; """); 
      
-  cqlCommand(c, """
+  command(c, """
      UPDATE songs  SET tags = tags + {'1973'}
        WHERE id = a3e64f8f-bd44-4f28-b8d9-6938726e34d4; """); 
      
-  cqlCommand(c, """
+  command(c, """
      UPDATE songs  SET tags = tags + {'blues'}
        WHERE id = a3e64f8f-bd44-4f28-b8d9-6938726e34d4; """); 
      
-  cqlCommand(c, """
+  command(c, """
      UPDATE songs  SET tags = tags + {'rock'}
        WHERE id = 7db1a490-5878-11e2-bcfd-0800200c9a66; """); 
   
-  cqlCommand(c, 
+  command(c, 
     "ALTER TABLE songs ADD reviews list<text>;");
 
-  cqlCommand(c, 
+  command(c, 
     "ALTER TABLE songs ADD venue map<timestamp, text>;");
   
-  cqlCommand(c, """
+  command(c, """
     UPDATE songs
       SET tags = tags + {'rock'}
       WHERE id = 7db1a490-5878-11e2-bcfd-0800200c9a66;
     """);
 
-  cqlCommand(c, """
+  command(c, """
     UPDATE songs
       SET reviews = reviews + [ 'hot dance music' ]
       WHERE id = 7db1a490-5878-11e2-bcfd-0800200c9a66;
     """);
 
-  cqlCommand(c, """
+  command(c, """
     INSERT INTO songs (id, venue)
       VALUES (7db1a490-5878-11e2-bcfd-0800200c9a66, 
               { '2013-9-22 12:01' : 'The Fillmore', 
                 '2013-10-1 18:00' : 'The Apple Barrel'});
     """);
 
-  println(cqlQuery(c, "SELECT * FROM SONGS"));
-  println(cqlQuery(c, "SELECT id, tags FROM songs;"));
-  println(cqlQuery(c, "SELECT id, venue FROM songs;"));
+  println(query(c, "SELECT * FROM SONGS"));
+  println(query(c, "SELECT id, tags FROM songs;"));
+  println(query(c, "SELECT id, venue FROM songs;"));
 
-  cqlCommand(c, 
+  command(c, 
     "CREATE INDEX album_name ON playlists ( album );");
-  cqlCommand(c, 
+  command(c, 
     "CREATE INDEX title_name ON playlists ( title );");
 
-  println(cqlQuery(c, """
+  println(query(c, """
     SELECT * FROM playlists
       WHERE album = 'Roll Away' 
         AND title = 'Outside Woman Blues'
       ALLOW FILTERING ; """));
   
-  cqlCommand(c, """
+  command(c, """
     UPDATE songs
       SET title = 'NN'
       WHERE id = 8a172618-b121-4136-bb10-f665cfc469eb;""");
   
-  println(cqlQuery(c, """
+  println(query(c, """
     SELECT WRITETIME (title)
       FROM songs
       WHERE id = 8a172618-b121-4136-bb10-f665cfc469eb;"""));
@@ -515,50 +515,50 @@ function test01()
   
   ## clean up ##
 
-  cqlCommand(c, "DROP TABLE songs;");
-  cqlCommand(c, "DROP TABLE playlists;");
-  cqlCommand(c, "DROP KEYSPACE demo;");
+  command(c, "DROP TABLE songs;");
+  command(c, "DROP TABLE playlists;");
+  command(c, "DROP KEYSPACE demo;");
 
-  cqlDisconnect(c);
+  disconnect(c);
   nothing
 end
 
 function test02()
-  c = cqlConnect("tilient.net");
+  c = connect("tilient.net");
 
-  cqlCommand(c, "CREATE KEYSPACE demo 
+  command(c, "CREATE KEYSPACE demo 
                  WITH REPLICATION = {'class' : 'SimpleStrategy',
                                      'replication_factor' : 1};");
-  cqlCommand(c, "USE DEMO;");
-  cqlCommand(c, "create table person (id int primary key, 
+  command(c, "USE DEMO;");
+  command(c, "create table person (id int primary key, 
                                       name varchar); ");
 
   ## Fast, Asynchronous inserts ##
   @time begin
     for id in 1:17000
-      cqlAsyncCommand(c, 
+      asyncCommand(c, 
         "INSERT INTO person (id, name) VALUES ($id, 'hihaho');");
     end
-    cqlSync(c);
+    sync(c);
   end
 
-  println(cqlQuery(c, "SELECT count(*) from person;")[1][1]);
+  println(query(c, "SELECT count(*) from person;")[1][1]);
 
-  cqlCommand(c, "DROP TABLE person;");
-  cqlCommand(c, "DROP KEYSPACE demo;");
-  cqlDisconnect(c);
+  command(c, "DROP TABLE person;");
+  command(c, "DROP KEYSPACE demo;");
+  disconnect(c);
   nothing
 end
 
 function test03()
-  c = cqlConnect("tilient.net");
+  c = connect("tilient.net");
 
-  cqlCommand(c, "CREATE KEYSPACE demo 
+  command(c, "CREATE KEYSPACE demo 
                  WITH REPLICATION = 
                    {'class' : 'SimpleStrategy',
                     'replication_factor' : 1};");
-  cqlCommand(c, "USE DEMO;");
-  cqlCommand(c, "create table person (id int , 
+  command(c, "USE DEMO;");
+  command(c, "create table person (id int , 
                                       od int,
                                       name varchar, 
                               PRIMARY KEY (id, od)); ");
@@ -567,22 +567,22 @@ function test03()
   
   @time begin
     for i in 1:N
-      cqlAsyncCommand(c, "INSERT INTO person (id, od, name) 
+      asyncCommand(c, "INSERT INTO person (id, od, name) 
                            VALUES (1, $i, 'hihaho');");
     end
-    cqlSync(c);
+    sync(c);
   end
 
   @time begin
-    cqlQuery(c, "select id, od from person 
+    query(c, "select id, od from person 
                   where id = 1
                   order by od asc limit $N;");
     nothing
   end
 
-  cqlCommand(c, "DROP TABLE person;");
-  cqlCommand(c, "DROP KEYSPACE demo;");
-  cqlDisconnect(c);
+  command(c, "DROP TABLE person;");
+  command(c, "DROP KEYSPACE demo;");
+  disconnect(c);
   nothing
 end
 
